5e8375593206355a80e523151531fe20dd2706ef
Guilhem <buisanguilhem@gmail.com>
2018-01-10 17:05:40 +0100

    Adds dynamic reconfigure

--- include/MorseHanpBridge.h
+++ include/MorseHanpBridge.h
@@ -14,11 +14,21 @@
 #include "visualization_msgs/MarkerArray.h"
 #include "tf/tf.h"
 #include "signal.h"
+#include <tf/transform_listener.h>
+
+#include "morse_hanp_bridge/morse_hanp_bridgeConfig.h"
+#include "dynamic_reconfigure/server.h"
 
 #define SUB_TOPIC "/human_pose"
 #define PUB_TOPIC "/tracked_human_pose"
 #define MARKER_PUB_TOPIC "/tracked_human_pose_markers"
 #define PUB_RATE 10
+
+typedef struct {
+    bool restrain_to_robot_FOV;
+    double robot_FOV_angle;
+    double robot_FOV_range;
+}morse_hanp_bridgeCfg;
 
 class MorseHanpBridge{
 
@@ -36,6 +46,8 @@ public:
     geometry_msgs::TwistStamped currentVelocity;
     bool isLastVelocity;
     geometry_msgs::TwistStamped lastVelocity;
+    morse_hanp_bridgeCfg config;
+    tf::TransformListener listener;
 
     MorseHanpBridge();
     ~MorseHanpBridge();
@@ -43,6 +55,8 @@ public:
     void fillVelocity (hanp_msgs::TrackedSegment &segment);
     void fillAcceleration (hanp_msgs::TrackedSegment &segment);
     void humanPoseCallback(const geometry_msgs::PoseStamped& poseMsg);
+    void dynConfigCallback(morse_hanp_bridge::morse_hanp_bridgeConfig &config, uint32_t level);
+    void fillAndSendHumans();
 
 
 
--- src/morse_hanp_bridge_node.cpp
+++ src/morse_hanp_bridge_node.cpp
@@ -1,3 +1,4 @@
+#include <MorseHanpBridge.h>
 #include "MorseHanpBridge.h"
 
 MorseHanpBridge::~MorseHanpBridge() = default;
@@ -11,67 +12,89 @@ MorseHanpBridge::MorseHanpBridge() {
     markerPub = n.advertise<visualization_msgs::MarkerArray>(MARKER_PUB_TOPIC, 1);
 }
 
+void MorseHanpBridge::fillAndSendHumans() {
+    auto now = ros::Time::now();
+    hanp_msgs::TrackedHumans trackedHumansMsg;
+    hanp_msgs::TrackedHuman trackedHuman;
+    hanp_msgs::TrackedSegment trackedSegment;
+    visualization_msgs::MarkerArray humans_markers;
+    trackedSegment.type = hanp_msgs::TrackedSegmentType::TORSO;
+    trackedSegment.pose.pose = currentPose.pose;
+    fillVelocity(trackedSegment);
+    if (isLastVelocity) {
+        fillAcceleration(trackedSegment);
+        trackedHuman.track_id = 3;
+        trackedHuman.segments.push_back(trackedSegment);
+        trackedHumansMsg.humans.push_back(trackedHuman);
+        trackedHumansMsg.header.frame_id = "optitrack";
+
+        visualization_msgs::Marker human_arrow, human_cylinder;
+
+        human_arrow.header.stamp = now;
+        human_arrow.header.frame_id = "optitrack";
+        human_arrow.type = visualization_msgs::Marker::ARROW;
+        human_arrow.action = visualization_msgs::Marker::MODIFY;
+        human_arrow.id = 103;
+        human_arrow.pose.position.x = trackedSegment.pose.pose.position.x;
+        human_arrow.pose.position.y = trackedSegment.pose.pose.position.y;
+        human_arrow.pose.orientation = tf::createQuaternionMsgFromYaw(
+                tf::getYaw(trackedSegment.pose.pose.orientation));
+        human_arrow.scale.x = 0.25 * 2.0;
+        human_arrow.scale.y = 0.1;
+        human_arrow.scale.z = 0.1;
+        human_arrow.color.a = 1.0;
+        human_arrow.color.r = 0;
+        human_arrow.color.g = 150;
+        human_arrow.color.b = 200;
+        human_arrow.lifetime = ros::Duration(4.0);
+
+        human_cylinder.header.stamp = now;
+        human_cylinder.header.frame_id = "optitrack";
+        human_cylinder.type = visualization_msgs::Marker::CYLINDER;
+        human_cylinder.action = visualization_msgs::Marker::MODIFY;
+        human_cylinder.id = 3;
+        human_cylinder.pose.position.x = trackedSegment.pose.pose.position.x;
+        human_cylinder.pose.position.y = trackedSegment.pose.pose.position.y;
+        human_cylinder.pose.position.z += (1.5 / 2);
+        // human_cylinder.pose.orientation = body_segment.pose.orientation;
+        human_cylinder.scale.x = 0.25 * 2;
+        human_cylinder.scale.y = 0.25 * 2;
+        human_cylinder.scale.z = 1.5;
+        human_cylinder.color.a = 1.0;
+        human_cylinder.color.r = 0;
+        human_cylinder.color.g = 150;
+        human_cylinder.color.b = 200;
+        human_cylinder.lifetime = ros::Duration(4.0);
+
+        humans_markers.markers.push_back(human_arrow);
+        humans_markers.markers.push_back(human_cylinder);
+
+        trackedHumansMsg.header.stamp = now;
+
+        humanPub.publish(trackedHumansMsg);
+        markerPub.publish(humans_markers);
+    }
+}
+
 void MorseHanpBridge::publishHumans(const ros::TimerEvent &e) {
-    if (isLastPose) {
-        auto now = ros::Time::now();
-        hanp_msgs::TrackedHumans trackedHumansMsg;
-        hanp_msgs::TrackedHuman trackedHuman;
-        hanp_msgs::TrackedSegment trackedSegment;
-        visualization_msgs::MarkerArray humans_markers;
-        trackedSegment.type = hanp_msgs::TrackedSegmentType::TORSO;
-        trackedSegment.pose.pose = currentPose.pose;
-        fillVelocity(trackedSegment);
-        if (isLastVelocity) {
-            fillAcceleration(trackedSegment);
-            trackedHuman.track_id = 3;
-            trackedHuman.segments.push_back(trackedSegment);
-            trackedHumansMsg.humans.push_back(trackedHuman);
-            trackedHumansMsg.header.frame_id = "optitrack";
-
-            visualization_msgs::Marker human_arrow, human_cylinder;
-
-            human_arrow.header.stamp = now;
-            human_arrow.header.frame_id = "optitrack";
-            human_arrow.type = visualization_msgs::Marker::ARROW;
-            human_arrow.action = visualization_msgs::Marker::MODIFY;
-            human_arrow.id = 103;
-            human_arrow.pose.position.x = trackedSegment.pose.pose.position.x;
-            human_arrow.pose.position.y = trackedSegment.pose.pose.position.y;
-            human_arrow.pose.orientation = tf::createQuaternionMsgFromYaw(tf::getYaw(trackedSegment.pose.pose.orientation));
-            human_arrow.scale.x = 0.25 * 2.0;
-            human_arrow.scale.y = 0.1;
-            human_arrow.scale.z = 0.1;
-            human_arrow.color.a = 1.0;
-            human_arrow.color.r = 0;
-            human_arrow.color.g = 150;
-            human_arrow.color.b = 200;
-            human_arrow.lifetime = ros::Duration(4.0);
-
-            human_cylinder.header.stamp = now;
-            human_cylinder.header.frame_id = "optitrack";
-            human_cylinder.type = visualization_msgs::Marker::CYLINDER;
-            human_cylinder.action = visualization_msgs::Marker::MODIFY;
-            human_cylinder.id = 3;
-            human_cylinder.pose.position.x = trackedSegment.pose.pose.position.x;
-            human_cylinder.pose.position.y = trackedSegment.pose.pose.position.y;
-            human_cylinder.pose.position.z += (1.5 / 2);
-            // human_cylinder.pose.orientation = body_segment.pose.orientation;
-            human_cylinder.scale.x = 0.25 * 2;
-            human_cylinder.scale.y = 0.25 * 2;
-            human_cylinder.scale.z = 1.5;
-            human_cylinder.color.a = 1.0;
-            human_cylinder.color.r = 0;
-            human_cylinder.color.g = 150;
-            human_cylinder.color.b = 200;
-            human_cylinder.lifetime = ros::Duration(4.0);
-
-            humans_markers.markers.push_back(human_arrow);
-            humans_markers.markers.push_back(human_cylinder);
-
-            trackedHumansMsg.header.stamp = now;
-
-            humanPub.publish(trackedHumansMsg);
-            markerPub.publish(humans_markers);
+    if (!config.restrain_to_robot_FOV) {
+        if (isLastPose) {
+                fillAndSendHumans();
+        }
+    }else{
+        geometry_msgs::PoseStamped humanPoseFromRobot;
+        try {
+            listener.transformPose("/base_link", currentPose, humanPoseFromRobot);
+        }catch (tf::TransformException &ex){
+            ROS_ERROR("%s", ex.what());
+            ros::Duration(1.0).sleep();
+            return;
+        }
+        double angle = std::atan2(humanPoseFromRobot.pose.position.y, humanPoseFromRobot.pose.position.x);
+        ROS_INFO("Human pose / robot frame : x=%f, y=%f; theta =%f", humanPoseFromRobot.pose.position.x, humanPoseFromRobot.pose.position.y, angle * 180 / 3.14159265);
+        if (hypot(humanPoseFromRobot.pose.position.x, humanPoseFromRobot.pose.position.y) <= config.robot_FOV_range
+                && angle <= config.robot_FOV_angle / 2 && angle >= -config.robot_FOV_angle / 2){
+            fillAndSendHumans();
         }
     }
 }
@@ -88,10 +111,10 @@ void MorseHanpBridge::fillVelocity(hanp_msgs::TrackedSegment &segment) {
                                  currentPose.pose.orientation.y,
                                  currentPose.pose.orientation.z,
                                  currentPose.pose.orientation.w)
-                          .inverse() * tf::Quaternion(lastPose.pose.orientation.x,
+                          * tf::Quaternion(lastPose.pose.orientation.x,
                                                       lastPose.pose.orientation.y,
                                                       lastPose.pose.orientation.z,
-                                                      lastPose.pose.orientation.w))
+                                                      lastPose.pose.orientation.w).inverse())
             .getRPY(roll_diff, pitch_diff, yaw_diff);
     lastVelocity = currentVelocity;
     isLastVelocity = isCurrentVelocity;
@@ -132,6 +155,13 @@ void MorseHanpBridge::humanPoseCallback(const geometry_msgs::PoseStamped &poseMs
     isCurrentPose = true;
 }
 
+void MorseHanpBridge::dynConfigCallback(morse_hanp_bridge::morse_hanp_bridgeConfig &newConfig, uint32_t level) {
+    config.restrain_to_robot_FOV = newConfig.restrain_to_robot_FOV;
+    config.robot_FOV_angle = newConfig.robot_FOV_angle;
+    config.robot_FOV_range = newConfig.robot_FOV_range;
+    ROS_INFO("Morse Hanp Bridge : reconfigured : %d, %f, %f", config.restrain_to_robot_FOV, config.robot_FOV_range, config.robot_FOV_angle);
+}
+
 void sigintHandler(int sig){
     ROS_DEBUG_STREAM("node will now shutdown");
     // the default sigint handler, it calls shutdown() on node
@@ -145,6 +175,10 @@ int main(int argc, char** argv){
     MorseHanpBridge mhb;
     mhb.publishTimer = mhb.n.createTimer(ros::Duration(0.1), &MorseHanpBridge::publishHumans, &mhb);
     mhb.poseSubscriber = mhb.n.subscribe(SUB_TOPIC, 1, &MorseHanpBridge::humanPoseCallback, &mhb);
+    dynamic_reconfigure::Server<morse_hanp_bridge::morse_hanp_bridgeConfig> server;
+    dynamic_reconfigure::Server<morse_hanp_bridge::morse_hanp_bridgeConfig>::CallbackType f;
+    f = boost::bind(&MorseHanpBridge::dynConfigCallback, &mhb, _1, _2);
+    server.setCallback(f);
     ROS_INFO("Morse Hanp Bridge launched");
     ros::spin();
 
